4a5493266c2d3e7ce8c1b78c2617ae7172b6739b
Buddhika85 <buddhika2006system@gmail.com>
2019-06-15 17:15:07 +0530

    Prism Framework - events

--- FriendOrganizer.UI/ViewModel/FriendDetailViewModel.cs
+++ FriendOrganizer.UI/ViewModel/FriendDetailViewModel.cs
@@ -3,18 +3,28 @@ using System.Threading.Tasks;
 using FriendOrganizer.Model;
 using FriendOrganizer.UI.Annotations;
 using FriendOrganizer.UI.Data;
+using FriendOrganizer.UI.Event;
+using Prism.Events;
 
 namespace FriendOrganizer.UI.ViewModel
 {
     public class FriendDetailViewModel : ViewModelBase, IFriendDetailViewModel
     {
-        private readonly IFriendDataService _friendDataService;
+        private IFriendDataService _friendDataService;
+        private IEventAggregator _eventAggregator;
 
         private Friend _friend;
 
-        public FriendDetailViewModel(IFriendDataService friendDataService)
+        public FriendDetailViewModel(IFriendDataService friendDataService, IEventAggregator eventAggregator)
         {
             _friendDataService = friendDataService;
+            _eventAggregator = eventAggregator;
+            _eventAggregator.GetEvent<OpenFriendDetailViewEvent>().Subscribe(OnOpenFriendDetailView);
+        }
+
+        private async void OnOpenFriendDetailView(int friendId)
+        {
+            await LoadAsync(friendId);
         }
 
         public async Task LoadAsync(int friendId)
--- FriendOrganizer.UI/ViewModel/NavigationViewModel.cs
+++ FriendOrganizer.UI/ViewModel/NavigationViewModel.cs
@@ -1,18 +1,39 @@
 ﻿using System.Collections.ObjectModel;
+using System.Data.Entity.Migrations.Model;
 using System.Threading.Tasks;
 using FriendOrganizer.Model;
 using FriendOrganizer.UI.Data;
+using FriendOrganizer.UI.Event;
+using Prism.Events;
 
 namespace FriendOrganizer.UI.ViewModel
 {
-    public class NavigationViewModel : INavigationViewModel
+    public class NavigationViewModel : ViewModelBase, INavigationViewModel
     {
         private IFriendLookupDataService _friendLookupDataService;
+        private IEventAggregator _eventAggregator;
+        
         public ObservableCollection<LookupItem> Friends { get; }
+        private LookupItem _selectedFriend;
 
-        public NavigationViewModel(IFriendLookupDataService friendLookupDataService)
+        public LookupItem SelectedFriend
+        {
+            get => _selectedFriend;
+            set
+            {
+                _selectedFriend = value;
+                OnPropertyChanged();
+                if (_selectedFriend != null)
+                {
+                    _eventAggregator.GetEvent<OpenFriendDetailViewEvent>().Publish(_selectedFriend.Id);
+                }
+            }
+        }
+
+        public NavigationViewModel(IFriendLookupDataService friendLookupDataService, IEventAggregator eventAggregator)
         {
             _friendLookupDataService = friendLookupDataService;
+            _eventAggregator = eventAggregator;
             Friends = new ObservableCollection<LookupItem>();
         }
 
